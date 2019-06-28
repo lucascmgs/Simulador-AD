448adb97dcbcfde0f1d6780eb132e35fd408e233
lucascmgs <lucascmgsds@gmail.com>
2019-06-28 02:46:52 +0000

    Escrevendo apenas ao fim da simulação

--- src/Rodada.cpp
+++ src/Rodada.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 //Construtor
 Rodada::Rodada(int indiceRodada, int tamanhoRodada, FilaMM1 *fila){ 
-    Escritor esc = Escritor();
+    //Escritor esc = Escritor();
     this->Indice = indiceRodada;
     this->Tamanho = tamanhoRodada;
     this->Fila = fila;
-    esc.CriaCSV();
+    //esc.CriaCSV();
 }
 
 
@@ -23,15 +23,15 @@ void Rodada::RealizaRodada(){
 void Rodada::ColetaResultadosDaRodada(){
     double tempoMedio = this->Fila->TempoMedioDeEsperaNaFila();
     double varianciaTempoMedio = this->Fila->VarianciaDoTempoDeEsperaNaFila();
-    Escritor esc = Escritor();
-    std::fstream arquivo;
+    //Escritor esc = Escritor();
+    //std::fstream arquivo;
 	this->EWRodada = tempoMedio;
 	this->EWRodada2 = pow(tempoMedio, 2);
 	this->VWRodada = varianciaTempoMedio;
 	this->VWRodada2 = pow(varianciaTempoMedio, 2);
-    std::vector<double> linha (4, 0.0);
-    linha.at(0) = this->EWRodada; linha.at(1) = this->EWRodada2; linha.at(2) = this->VWRodada; linha.at(3) = this->VWRodada2;
-    arquivo = esc.EscreveLinhaEmCSV(4, linha);
+    //std::vector<double> linha (4, 0.0);
+    //linha.at(0) = this->EWRodada; linha.at(1) = this->EWRodada2; linha.at(2) = this->VWRodada; linha.at(3) = this->VWRodada2;
+    //arquivo = esc.EscreveLinhaEmCSV(4, linha);
 
 	//std::cout << "---- FIM DA RODADA " << this->Indice << " ----" << std::endl;
 	//std::cout << "est coletadas: EWRodada,EWRodada2,VWRodada,VWRodada2" << std::endl;
@@ -40,6 +40,6 @@ void Rodada::ColetaResultadosDaRodada(){
 	//std::cout << "VWRodada: " << VWRodada << std::endl;
 	//std::cout << "VWRodada2: " << VWRodada2 << std::endl;
 
-    arquivo.close();
+    //arquivo.close();
     this->Fila->ResetaEstatisticasRodada();
 }
