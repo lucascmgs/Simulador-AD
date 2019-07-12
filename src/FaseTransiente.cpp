ea4b5f049c4ec78b7f39b7198c7f6b1cf7a78f23
Silvia Pimpão Vasquez <silviapimpaovasquez@gmail.com>
2019-07-12 07:34:06 -0300

    ajustes

--- src/FaseTransiente.cpp
+++ src/FaseTransiente.cpp
@@ -52,8 +52,8 @@ void FaseTransiente::RodaFaseTransiente(){
         } else{
             deltaEstavelNumeroPessoas++;
         }
-        //Se, em 1000 eventos tratados em sequência, nenhum delta mudar, teremos terminado nossa fase transiente
-        if(deltaEstavelTempoEspera > 1000 && deltaEstavelNumeroPessoas > 1000){
+        //Se, em 2877 eventos tratados em sequência, nenhum delta mudar, teremos terminado nossa fase transiente
+        if(deltaEstavelTempoEspera > 2877 && deltaEstavelNumeroPessoas > 2877){
             transiente = false;
         } else {
             //Atualizamos as métricas para os eventos anteriores
